252d7ae62e762c00ffec81be992f5f1b0b3d6637
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-12-17 14:58:46 +0900

    Update ios15_kancolle.js

--- ios15_kancolle.js
+++ ios15_kancolle.js
@@ -3,24 +3,23 @@ class iOS15_kancolle {
     if(gadgetInfo === undefined) return alart("艦これのゲームページで実行してください");
     window.open("http:"+gadgetInfo.URL,'_blank');
   }
-  opensupport(){
-  d=document;
-  s=d.createElement('link');
+  opensupport(document){
+  s=document.createElement('link');
   s.rel='stylesheet';
   s.href='https://iOS15-kancolle.github.io/iframe.css';
-  d.body.appendChild(s);
+  document.body.appendChild(s);
   s=document.createElement('br');
-  d.body.appendChild(s);
+  document.body.appendChild(s);
   s=document.createElement('iframe');
   s.src='http://kancollecalc.jp/air_supremacy.html';
   s.width='50%';
   s.height='50%';
-  d.body.appendChild(s);
+  document.body.appendChild(s);
   s=document.createElement('iframe');
   s.src='http://kc2ndexpcalc.web.fc2.com';
   s.width='50%';
   s.height='50%';
-  d.body.appendChild(s);
+  document.body.appendChild(s);
   }
 };
 window.iOS15_kancolle = iOS15_kancolle;
