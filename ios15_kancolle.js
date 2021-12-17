249de20c1e4d666e068058663d4e59538eb62373
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-12-17 15:04:18 +0900

    Update ios15_kancolle.js

--- ios15_kancolle.js
+++ ios15_kancolle.js
@@ -4,22 +4,22 @@ class iOS15_kancolle {
     window.open("http:"+gadgetInfo.URL,'_blank');
   }
   opensupport(document){
-  s=document.createElement('link');
-  s.rel='stylesheet';
-  s.href='https://iOS15-kancolle.github.io/iframe.css';
-  document.body.appendChild(s);
-  s=document.createElement('br');
-  document.body.appendChild(s);
-  s=document.createElement('iframe');
-  s.src='http://kancollecalc.jp/air_supremacy.html';
-  s.width='50%';
-  s.height='50%';
-  document.body.appendChild(s);
-  s=document.createElement('iframe');
-  s.src='http://kc2ndexpcalc.web.fc2.com';
-  s.width='50%';
-  s.height='50%';
-  document.body.appendChild(s);
+    var s=document.createElement('link');
+    s.rel='stylesheet';
+    s.href='https://iOS15-kancolle.github.io/iframe.css';
+    document.body.appendChild(s);
+    s=document.createElement('br');
+    document.body.appendChild(s);
+    s=document.createElement('iframe');
+    s.src='http://kancollecalc.jp/air_supremacy.html';
+    s.width='50%';
+    s.height='50%';
+    document.body.appendChild(s);
+    s=document.createElement('iframe');
+    s.src='http://kc2ndexpcalc.web.fc2.com';
+    s.width='50%';
+    s.height='50%';
+    document.body.appendChild(s);
   }
 };
 window.iOS15_kancolle = iOS15_kancolle;
