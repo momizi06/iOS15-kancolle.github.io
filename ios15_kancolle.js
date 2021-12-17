560b9880df9de0b0f7aa391f44f6d271cf0c6f5f
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-12-17 16:01:49 +0900

    Update ios15_kancolle.js

--- ios15_kancolle.js
+++ ios15_kancolle.js
@@ -8,21 +8,16 @@ class iOS15_kancolle {
     s.rel='stylesheet';
     s.href='https://iOS15-kancolle.github.io/iframe.css';
     document.body.appendChild(s);
-    s=document.createElement('div');
-    s.id='kancolle_support_site';
-    s.width='100%';
-    s.height='50%';
-    document.body.appendChild(s);
     s=document.createElement('iframe');
     s.src='http://kancollecalc.jp/air_supremacy.html';
     s.width='48%';
     s.height='100%';
-    document.getElementById('kancolle_support_site').appendChild(s);
+    document.body.appendChild(s);
     s=document.createElement('iframe');
     s.src='http://kc2ndexpcalc.web.fc2.com';
     s.width='48%';
     s.height='100%';
-    document.getElementById('kancolle_support_site').appendChild(s);
+    document.body.appendChild(s);
   }
 };
 window.iOS15_kancolle = iOS15_kancolle;
