cadac390799f0d7aa6a3c4a31dbf3e9d2914130c
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-03-03 14:17:33 +0900

    Update ios15_kancolle.js

--- ios15_kancolle.js
+++ ios15_kancolle.js
@@ -1,6 +1,6 @@
 class iOS15_kancolle {
   open(){
-    if(gadgetInfo === undefined) return alart("艦これのゲームページで実行してください");
+    if(typeof gadgetInfo === undefined) return alart("艦これのゲームページで実行してください");
     window.open("http:"+gadgetInfo.URL,'_blank');
   }
   opensupport(document){
