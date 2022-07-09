71e4a6477414b3383fe33ca84c970e7881878df2
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-07-09 11:31:51 +0900

    Update ios15_kancolle_beta.js

--- ios15_kancolle_beta.js
+++ ios15_kancolle_beta.js
@@ -5,51 +5,47 @@ class iOS15_kancolle {
     var W=open();
     W.document.write(F);
     W.document.close();
-    /*
     var H=`<html>
            <!-- TODO LOAD JQUERY -->
            <script>
-           (($,_)=>{
-             const html=$.documentElement,
-                   gf=$.getElementById('htmlWrap'),
-                   gs=gf.style,
-                   gw=gf.offsetWidth,
-                   gh=gw*.6;
-             let vp=$.querySelector('meta[name=viewport]'),
-                 t=0;
-             vp||(vp=$.createElement('meta'),
-             vp.name='viewport',
-             $.querySelector('head').appendChild(vp));
-             vp.content='width='+gw;
-             'orientation'in _&&html.webkitRequestFullscreen&&html.webkitRequestFullscreen();
-             html.style.overflow='hidden';
-             $.body.style.cssText='min-width:0;overflow:hidden;margin:0';
-             gs.position='fixed';
-             gs.marginRight='auto';
-             gs.marginLeft='auto';
-             gs.top='-5px';
-             gs.right='0';
-             gs.zIndex='100';
-             gs.transformOrigin='50%25 16px';
-             if(!_.kancolleFit){
-               const k=()=>{
-                 const w=html.clientWidth,
-                       h=_.innerHeight;
-                 w/h<1/.6?gs.transform='scale(' w/gw ')':gs.transform='scale(' h/gh ')';w<gw?gs.left='-' (gw-w)/2 'px':gs.left='0'};
-                 _.addEventListener('resize',()=>{
-                   clearTimeout(t);
-                   t=setTimeout(k,10)
-                 });
-                 _.kancolleFit=k;
-               }
-               kancolleFit();
-             })(parent.frames[0].document,window)
+           (($, _) => {
+  　　　　　    const html = $.documentElement,
+        gf = $.getElementById('htmlWrap'),
+        gs = gf.style,
+        gw = gf.offsetWidth,
+        gh = gw * .6;
+      let vp = $.querySelector('meta[name=viewport]'),
+        t = 0;
+      vp || (vp = $.createElement('meta'), vp.name = 'viewport', $.querySelector('head').appendChild(vp));
+      vp.content = 'width=' + gw;
+      'orientation' in _ && html.webkitRequestFullscreen && html.webkitRequestFullscreen();
+      html.style.overflow = 'hidden';
+      $.body.style.cssText = 'min-width:0;overflow:hidden;margin:0';
+      gs.position = 'fixed';
+      gs.marginRight = 'auto';
+      gs.marginLeft = 'auto';
+      gs.top = '-5px';
+      gs.right = '0';
+      gs.zIndex = '100';
+      gs.transformOrigin = '50% 16px';
+      if (!_.kancolleFit) {
+        const k = () => {
+          const w = html.clientWidth,
+            h = _.innerHeight;
+          w / h < 1 / .6 ? gs.transform = 'scale(' + w / gw + ')' : gs.transform = 'scale(' + h / gh + ')';
+          w < gw ? gs.left = '-' + (gw - w) / 2 + 'px' : gs.left = '0'
+        };
+            _.addEventListener('resize', () => {
+          clearTimeout(t);
+          t = setTimeout(k, 10)
+        });
+        _.kancolleFit = k
+      }
+      kancolleFit()
+    })(parent.frames[0].document,window)
            </script>
          </html>`;
          W.frames[1].document.write(H);
-         W.frames[1].document.close();
-         */
-         W.frames[1].document.write(`<script>alert("hello world");</script>`);
          W.frames[1].document.close();
     })();
   }
